f13da7b7c03c4a371fa043001b7f41643c83cb0f
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-11-24 23:04:47 -0600

    Add basic OLED code

--- config.h
+++ config.h
@@ -15,6 +15,10 @@
  */
 
 #pragma once
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
 #include "config-user.h"
 
 #ifdef PLANCK
@@ -142,10 +146,19 @@ LAYOUT_planck_grid( \
        K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29, \
                      K30,   K31,   K32,   K33,   K34,   K35 \
 ) \
-LAYOUT_split_3x6_3( \
+LAYOUT_split_3x5_3( \
   K00,   K01,   K02, K03, K04,  K05, K06, K07, K08,   K09, \
   K10,   K11,   K12, K13, K14,  K15, K16, K17, K18,   K19, \
   K20,   K21,   K22, K23, K24,  K25, K26, K27, K28,   K29, \
   K30, K31, K32,  K33, K34, K35 \
 )
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define USE_SERIAL_PD2
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+
 #endif // CRKBD
