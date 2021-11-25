937ffe3f763347c48aae04575cfafc64f2c26e99
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-11-24 22:10:39 -0600

    Put planck rules in

--- rules.mk
+++ rules.mk
@@ -1,10 +1,3 @@
-SRC += muse.c
-# Set any rules.mk overrides for your specific keymap here.
-# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-WEBUSB_ENABLE = no
-ORYX_ENABLE = no
-TAP_DANCE_ENABLE = yes
-MOUSEKEY_ENABLE = yes
+include keyboards/crkbd/mattfidler/crkbd.mk
+#include keyboards/planck/mattfidler/planck.mk
 
