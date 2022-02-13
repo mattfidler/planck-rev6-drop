82d0b2de9857ce527a4afea1cbe2f77e3a008a1b
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2022-02-13 00:23:49 -0600

    Drop tapdance

--- keymap.c
+++ keymap.c
@@ -180,129 +180,6 @@ enum planck_keycodes {
 #define HM_I RALT_T(KC_I)
 #define HM_O RGUI_T(KC_O)
 
-
-
-
-
-// Tap Dance declarations
-enum {
-    TD_J,
-    TD_K,
-    TD_V,
-};
-
-typedef enum {
-    TD_NONE,
-    TD_UNKNOWN,
-    TD_SINGLE_TAP,
-    TD_SINGLE_HOLD,
-    TD_DOUBLE_TAP,
-    TD_DOUBLE_HOLD,
-    TD_DOUBLE_SINGLE_TAP, // Send two single taps
-    TD_TRIPLE_TAP,
-    TD_TRIPLE_HOLD
-} td_state_t;
-
-typedef struct {
-    bool is_press_action;
-    td_state_t state;
-} td_tap_t;
-
-
-td_state_t cur_dance(qk_tap_dance_state_t *state);
-
-// For the x tap dance. Put it here so it can be used in any keymap
-void v_finished(qk_tap_dance_state_t *state, void *user_data);
-void v_reset(qk_tap_dance_state_t *state, void *user_data);
-
-// Tap Dance definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lock
-    [TD_J] = ACTION_TAP_DANCE_DOUBLE(KC_J, KC_LGUI),
-    [TD_K] = ACTION_TAP_DANCE_DOUBLE(KC_K, KC_APPLICATION),
-    [TD_V] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, v_finished, v_reset),
-};
-
-/* Return an integer that corresponds to what kind of tap dance should be executed.
- *
- * How to figure out tap dance state: interrupted and pressed.
- *
- * Interrupted: If the state of a dance dance is "interrupted", that means that another key has been hit
- *  under the tapping term. This is typically indicitive that you are trying to "tap" the key.
- *
- * Pressed: Whether or not the key is still being pressed. If this value is true, that means the tapping term
- *  has ended, but the key is still being pressed down. This generally means the key is being "held".
- *
- * One thing that is currenlty not possible with qmk software in regards to tap dance is to mimic the "permissive hold"
- *  feature. In general, advanced tap dances do not work well if they are used with commonly typed letters.
- *  For example "A". Tap dances are best used on non-letter keys that are not hit while typing letters.
- *
- * Good places to put an advanced tap dance:
- *  z,q,x,j,k,v,b, any function key, home/end, comma, semi-colon
- *
- * Criteria for "good placement" of a tap dance key:
- *  Not a key that is hit frequently in a sentence
- *  Not a key that is used frequently to double tap, for example 'tab' is often double tapped in a terminal, or
- *    in a web form. So 'tab' would be a poor choice for a tap dance.
- *  Letters used in common words as a double. For example 'p' in 'pepper'. If a tap dance function existed on the
- *    letter 'p', the word 'pepper' would be quite frustating to type.
- *
- * For the third point, there does exist the 'TD_DOUBLE_SINGLE_TAP', however this is not fully tested
- *
- */
-td_state_t cur_dance(qk_tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
-        // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
-        else return TD_SINGLE_HOLD;
-    } else if (state->count == 2) {
-        // TD_DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
-        // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
-        // keystrokes of the key, and not the 'double tap' action/macro.
-        if (state->interrupted) return TD_DOUBLE_SINGLE_TAP;
-        else if (state->pressed) return TD_DOUBLE_HOLD;
-        else return TD_DOUBLE_TAP;
-    }
-
-    // Assumes no one is trying to type the same letter three times (at least not quickly).
-    // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
-    // an exception here to return a 'TD_TRIPLE_SINGLE_TAP', and define that enum just like 'TD_DOUBLE_SINGLE_TAP'
-    if (state->count == 3) {
-        if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
-        else return TD_TRIPLE_HOLD;
-    } else return TD_UNKNOWN;
-}
-
-// Create an instance of 'td_tap_t' for the 'v' tap dance.
-static td_tap_t vtap_state = {
-    .is_press_action = true,
-    .state = TD_NONE
-};
-
-void v_finished(qk_tap_dance_state_t *state, void *user_data) {
-    vtap_state.state = cur_dance(state);
-    switch (vtap_state.state) {
-    case TD_SINGLE_TAP: register_code(KC_V); break;
-    case TD_SINGLE_HOLD: layer_on(_CA_NUM_R); break;
-    case TD_DOUBLE_TAP: register_code16(LALT(KC_F4)); break;
-    case TD_DOUBLE_HOLD: register_code(KC_V); break;
-    case TD_DOUBLE_SINGLE_TAP: tap_code(KC_V); register_code(KC_V);
-    default: break;
-    }
-}
-
-void v_reset(qk_tap_dance_state_t *state, void *user_data) {
-    switch (vtap_state.state) {
-    case TD_SINGLE_TAP: unregister_code(KC_V); break;
-    case TD_SINGLE_HOLD: layer_off(_CA_NUM_R); break;
-    case TD_DOUBLE_TAP: unregister_code16(LALT(KC_F4)); break;
-    case TD_DOUBLE_HOLD: unregister_code(KC_V);
-    case TD_DOUBLE_SINGLE_TAP: unregister_code(KC_V);
-    default: break;
-    }
-   vtap_state.state = TD_NONE;
-}
-
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
   case HM_T:
@@ -313,19 +190,14 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   }
 }
 
-#define D_J TD(TD_J)
-#define D_K TD(TD_K)
-#define D_V TD(TD_V)
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK] = LAYOUT_matt(
  //,---------------------------------------.                       ,---------------------------------------.
-       KC_Q,   KC_W,   LT_F,   LT_P,   KC_G,                           D_J,   LT_L,   LT_U,   KC_Y,  Q_QUO,
+       KC_Q,   KC_W,   LT_F,   LT_P,   KC_G,                           KC_J,   LT_L,   LT_U,   KC_Y,  Q_QUO,
  //|-------+-------+-------+-------+-------|                       |-------+-------+-------+-------+-------|
         Q_A,    Q_R,    Q_S,    Q_T,   KC_D,                           KC_H,    Q_N,    Q_E,    Q_I,    Q_O,
  //|-------+-------+-------+-------+-------|                       |-------+-------+-------+-------+-------|
-       KC_Z,    Q_X,   KC_C,    D_V,   KC_B,                            D_K,   LT_M,  Q_CMA,  Q_DOT,Q_SLASH,
+       KC_Z,    Q_X,   KC_C,   LT_V,   KC_B,                           KC_K,   LT_M,  Q_CMA,  Q_DOT,Q_SLASH,
  //|-------+-------+-------+-------+-------+-------|       |-------+-------+-------+-------+-------+-------|
                              LT_DEL, LT_BSP, LT_RET,         LT_TAB, LT_SPC, LT_ESC
  //                        |-------+-------+-------|       |-------+-------+-------|
